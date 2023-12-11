92de7ba43946831c212d41c266eeac1cb34b5368
KiUkKim <zidh1@korea.ac.kr>
2023-12-11 15:08:40 +0900

    [INIT] Project Configuration

---
+++ src/main/java/com/JangKi/dducksang/DducksangApplication.java
@@ -0,0 +1,13 @@
+package com.JangKi.dducksang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DducksangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DducksangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JangKi/dducksang/DducksangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JangKi.dducksang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DducksangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
