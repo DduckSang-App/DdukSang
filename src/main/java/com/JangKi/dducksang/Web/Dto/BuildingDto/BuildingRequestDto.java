ccd13763ede26c4d1ba0867285fc53b4f61b03e8
KiUkKim <zidh1@korea.ac.kr>
2024-01-09 17:50:45 +0900

    fix : DDUK1-34 BuildingRequest 수정 1. 기존 BuildingRequestDto 1 : N 방식으로 수정

---
+++ src/main/java/com/JangKi/dducksang/Web/Dto/BuildingDto/BuildingRequestDto.java
@@ -0,0 +1,63 @@
+package com.JangKi.dducksang.Web.Dto.BuildingDto;
+
+import com.JangKi.dducksang.domain.Address.Repository.Address;
+import com.JangKi.dducksang.domain.Building.Repository.Building;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Year;
+
+public class BuildingRequestDto {
+
+    @Getter
+    @NoArgsConstructor
+    public static class BuildingSaveDto{
+
+        private Year buildYear;
+
+        private int sigunguCode;
+
+        private int eupmyundongCode;
+
+        private String sigungu;
+
+        private String dong;
+
+        private String aptName;
+
+        private Address addressID;
+
+        public BuildingSaveDto(Year buildYear, int sigunguCode, int eupmyundongCode, String sigungu, String dong, String aptName)
+        {
+            this.buildYear = buildYear;
+            this.sigunguCode = sigunguCode;
+            this.eupmyundongCode = eupmyundongCode;
+            this.sigungu = sigungu;
+            this.dong = dong;
+            this.aptName = aptName;
+        }
+
+        public void setAddressID(Address addressID)
+        {
+            this.addressID = addressID;
+        }
+
+        @NotNull
+        public Building toEntity()
+        {
+            return  Building.builder()
+                    .buildYear(buildYear)
+                    .sigunguCode(sigunguCode)
+                    .eupmyundongCode(eupmyundongCode)
+                    .sigungu(sigungu)
+                    .dong(dong)
+                    .aptName(aptName)
+                    .address(addressID)
+                    .build();
+        }
+    }
+}
